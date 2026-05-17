04bf898e0c3f46f51e2e012d21a149205cfcd4e0
Irene Virgínia <isahun7@gmail.com>
2026-05-17 22:56:03 +0200

    feat: add auth, error and loading interceptors

---
+++ src/app/core/interceptors/error.interceptor.ts
@@ -0,0 +1,17 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
+
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  const router = inject(Router);
+  const auth = inject(AuthService);
+  return next(req).pipe(
+    catchError((err) => {
+      if (err.status === 401) auth.logout();
+      if (err.status === 500) console.error('Error servidor:', err.message);
+      return throwError(() => err);
+    }),
+  );
+};
