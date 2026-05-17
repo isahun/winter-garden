c5aa0220516c5a64139d2426138c9653b83748a0
Irene Virgínia <isahun7@gmail.com>
2026-05-17 22:55:45 +0200

    feat: add supabase service and environment config

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://aaohbzrkwypmyifkkuau.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFhb2hienJrd3lwbXlpZmtrdWF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkwMzg4NTcsImV4cCI6MjA5NDYxNDg1N30.8xa2NFmPSGMtY-75riMWuzNclJXDuI5DPfYgdigH7K0',
+};
