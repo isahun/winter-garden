a87c68ab25fc4a4d6f97e6465025226de546d917
Irene Virgínia <isahun7@gmail.com>
2026-05-17 22:58:39 +0200

    fix: remove unused Router injection from error interceptor

--- src/app/core/interceptors/error.interceptor.ts
+++ src/app/core/interceptors/error.interceptor.ts
@@ -1,11 +1,9 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { Router } from '@angular/router';
 import { catchError, throwError } from 'rxjs';
 import { AuthService } from '../auth/auth.service';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
-  const router = inject(Router);
   const auth = inject(AuthService);
   return next(req).pipe(
     catchError((err) => {
