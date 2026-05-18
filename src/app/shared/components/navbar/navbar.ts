2131d8ebc532612cfe72099fd180f7dfc6d4c9d0
Irene <isahun7@gmail.com>
2026-05-18 19:28:42 +0200

    refactor: use ng-template to avoid link duplication in navbar as per DRY principle

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,11 +1,12 @@
 import { Component, inject, signal } from '@angular/core';
+import { NgTemplateOutlet } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../../core/auth/auth.service';
 
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, NgTemplateOutlet],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
