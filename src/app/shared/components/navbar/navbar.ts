ee139765c12f5dacab41e7d79cabb15d2b352dbd
Irene <isahun7@gmail.com>
2026-05-18 12:56:57 +0200

    feat: add responsive navbar with hamburger menu and auth state

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../../core/auth/auth.service';
+
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
-
+  auth = inject(AuthService);
+  menuOpen = signal(false);
 }
