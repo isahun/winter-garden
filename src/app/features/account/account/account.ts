c409902e62f0aec040d9830830f8724c2acf634b
Irene Virgínia <isahun7@gmail.com>
2026-05-21 16:39:43 +0200

    feat(account): implement account hub and orders history page

--- src/app/features/account/account/account.ts
+++ src/app/features/account/account/account.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../../core/auth/auth.service';
 
 @Component({
   selector: 'app-account',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './account.html',
   styleUrl: './account.css',
 })
 export class Account {
-
+  auth = inject(AuthService);
 }
