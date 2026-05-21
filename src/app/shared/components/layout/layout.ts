0b459ad6d9d85d3e6c0dbfc415b254775162dd11
Irene Virgínia <isahun7@gmail.com>
2026-05-21 21:53:12 +0200

    feat(chatbot): integrate chatbot into main layout

--- src/app/shared/components/layout/layout.ts
+++ src/app/shared/components/layout/layout.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from '../navbar/navbar';
 import { Footer } from '../footer/footer';
+import { Chatbot } from '../chatbot/chatbot';
+
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet, Navbar, Footer],
+  imports: [RouterOutlet, Navbar, Footer, Chatbot],
   templateUrl: './layout.html',
   styleUrl: './layout.css'
 })
