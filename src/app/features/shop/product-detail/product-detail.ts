97b71dc2abb6f68a60df01d291b11963fc9904fd
Irene Virgínia <isahun7@gmail.com>
2026-05-22 22:10:56 +0200

    fix(shop): reload product detail on route param change using effect

--- src/app/features/shop/product-detail/product-detail.ts
+++ src/app/features/shop/product-detail/product-detail.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject, signal, input, numberAttribute } from '@angular/core';
+import { Component, inject, signal, input, numberAttribute, effect } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CurrencyPipe } from '@angular/common';
 import { ProductService } from '../../../core/services/product.service';
@@ -13,7 +13,7 @@ import { AuthService } from '../../../core/auth/auth.service';
   templateUrl: './product-detail.html',
   styleUrl: './product-detail.css',
 })
-export class ProductDetail implements OnInit {
+export class ProductDetail {
   id = input.required({ transform: numberAttribute });
 
   private productService = inject(ProductService);
@@ -26,14 +26,18 @@ export class ProductDetail implements OnInit {
   related = signal<Product[]>([]);
   lightboxOpen = signal(false);
 
-  async ngOnInit() {
-    const { data } = await this.productService.getProductById(this.id());
-    this.product.set(data);
-    if (data?.category_id) {
-      const { data: rel } = await this.productService.getRelatedProducts(data.category_id, data.id);
-      this.related.set(rel ?? []);
-    }
-    if (this.auth.isLoggedIn()) await this.favorites.loadFavorites();
+  constructor() {
+    effect(async () => {
+      const id = this.id();
+      const { data } = await this.productService.getProductById(id);
+      this.product.set(data);
+      this.related.set([]);
+      if (data?.category_id) {
+        const { data: rel } = await this.productService.getRelatedProducts(data.category_id, data.id);
+        this.related.set(rel ?? []);
+      }
+      if (this.auth.isLoggedIn()) await this.favorites.loadFavorites();
+    });
   }
 
   addProductToCart() {
