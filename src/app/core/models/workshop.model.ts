f8d0e2219dbf6a1527e46ae4f1002939c496d46b
Irene <isahun7@gmail.com>
2026-05-24 00:24:33 +0200

    fix(workshops): rename difficulty level Avançat to Expert

--- src/app/core/models/workshop.model.ts
+++ src/app/core/models/workshop.model.ts
@@ -1,4 +1,4 @@
-export type WorkshopDifficulty = 'Principiant' | 'Regular' | 'Avançat';
+export type WorkshopDifficulty = 'Principiant' | 'Regular' | 'Expert';
 
 export interface Workshop {
   id: number;
